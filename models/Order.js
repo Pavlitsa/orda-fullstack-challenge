f661c354d76925899f42a1be00521364d33211f8
Pavlitsa <pzafeiri@gmail.com>
2020-03-09 00:34:51 +0100

    fix schema

--- models/Order.js
+++ models/Order.js
@@ -1,13 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const orderSchema = new Schema({
-  sumTotal: Number,
-  time: Number,
-  tipSum: Number,
-  venueId: String,
-  venueName: Object
-});
+const orderSchema = new Schema({});
 
 const Order = mongoose.model("Order", orderSchema, "orda-orders");
 
